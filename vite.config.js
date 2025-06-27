5208bb73be15cd00e4394b0acd3b507968446431
Parth Agnihotri <parthagnihotri824@gmail.com>
2025-06-27 10:27:20 +0530

    fix: added base path for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+  base: './', // 🔥 Add this line here
   plugins: [
     react(),
   ],
